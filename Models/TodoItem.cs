695c4aa82fe346f7d3b2558e16a292b0229d2d73
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-03-13 14:20:55 +0000

    Add TodoItem model

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
