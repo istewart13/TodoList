7a2299b7d1c12a7a17973cd114c795892dd33d5e
Iain <iain.stewart08@gmail.com>
2019-03-19 22:51:11 +0000

    Remove FakeTodoItem service and replace with TodoItem service

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace AspNetCoreTodo
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+            services.AddScoped<ITodoItemService, TodoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
